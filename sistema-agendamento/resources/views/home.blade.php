87a6b78d9fc9ba9fd21404360dd19ebdbab7a26c
Danilo Farias de Sousa <danilofariasbkv@gmail.com>
2025-05-22 21:57:55 -0300

    uma melhoradinha na navbar

--- sistema-agendamento/resources/views/home.blade.php
+++ sistema-agendamento/resources/views/home.blade.php
@@ -1,11 +1,31 @@
 @extends('layout')
 
 @section('content')
-    <h1>Bem Vindo! - Sistema de agendamento</h1>
-    <ul>
-        <li><a href="/">Início</a></li>
-        <li><a href="/places">Meus Espaços</a></li>
-        <li><a href="/places/new">Novo Espaço</a></li>
-        <li><a href="/scheduling">Agendamentos</a></li>
-    </ul>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Início</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/places">Meus Espaços</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/places/new">Novo Espaço</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/scheduling">Agendamentos</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <div class="container mt-4">
+        <h1 class="text-center">Bem-vindo! - Sistema de Agendamento</h1>
+    </div>
+
 @endsection
