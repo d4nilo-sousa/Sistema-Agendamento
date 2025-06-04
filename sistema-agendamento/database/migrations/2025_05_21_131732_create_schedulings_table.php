15835c0819d1fbd72998947f7c40a280b3937f71
Danilo Farias de Sousa <danilofariasbkv@gmail.com>
2025-06-04 11:24:07 -0300

    header aprimorado

--- sistema-agendamento/database/migrations/2025_05_21_131732_create_schedulings_table.php
+++ sistema-agendamento/database/migrations/2025_05_21_131732_create_schedulings_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('schedulings', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->date('date');
+            $table->tinyInteger('class_number');
+            $table->tinyInteger('Shift');
         });
     }
 
