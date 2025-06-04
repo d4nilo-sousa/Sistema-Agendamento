6e2c5a67162c90c49d445d1d4a377db02490e259
Danilo Farias de Sousa <danilofariasbkv@gmail.com>
2025-06-04 11:14:24 -0300

    navbar aprimorada

--- sistema-agendamento/resources/views/home.blade.php
+++ sistema-agendamento/resources/views/home.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+<nav class="navbar navbar-expand-lg custom-navbar">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Início</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/places">Meus Espaços</a>
+          <a class="nav-link active" href="/places">Meus Espaços</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/places/new">Novo Espaço</a>
@@ -22,7 +23,7 @@
       </ul>
     </div>
   </div>
-</nav>
+</nav> 
 
     <div class="container mt-4">
         <h1 class="text-center">Bem-vindo! - Sistema de Agendamento</h1>
--- sistema-agendamento/resources/views/layout.blade.php
+++ sistema-agendamento/resources/views/layout.blade.php
@@ -5,10 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bootstrap demo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/custom_navbar.css') }}">
+
       </head>
 
       <body>
-        <h1>Hello, world!</h1>
         <main class="container">
           @yield('content')
         </main>   
